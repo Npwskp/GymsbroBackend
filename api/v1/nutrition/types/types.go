e8a390fdcd99324732200537bfffcbb3d7ce01d6
npwskp <nopawit.sukolpuk@gmail.com>
2024-11-21 21:34:33 +0700

    CamelCase json for meal

--- api/v1/nutrition/types/types.go
+++ api/v1/nutrition/types/types.go
@@ -1,15 +1,15 @@
 package types
 
 type Nutrient struct {
-	Name   string  `json:"name" validate:"required"`
-	Amount float64 `json:"amount" validate:"required"`
-	Unit   string  `json:"unit" validate:"required"`
+	Name   string  `json:"name" validate:"required" bson:"name"`
+	Amount float64 `json:"amount" validate:"required" bson:"amount"`
+	Unit   string  `json:"unit" validate:"required" bson:"unit"`
 }
 
 type Ingredient struct {
-	IngredientId  string  `json:"ingredientid" validate:"required" bson:"ingredientid"`
+	IngredientId  string  `json:"ingredientId" validate:"required" bson:"ingredientid"`
 	Name          string  `json:"name" validate:"required" bson:"name"`
-	Value         float64 `json:"value" validate:"required"`
-	Uint          string  `json:"unit" validate:"required"`
-	NumOfServings float64 `json:"numofservings" validate:"required"`
+	Value         float64 `json:"value" validate:"required" bson:"value"`
+	Uint          string  `json:"unit" validate:"required" bson:"unit"`
+	NumOfServings float64 `json:"numOfServings" validate:"required" bson:"num_of_servings"`
 }
